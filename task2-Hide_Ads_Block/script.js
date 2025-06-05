9aa67e05d8562924e50cf0041aa19c1f2627f997
ThienKhang410 <khangdt410@gmail.com>
2025-06-05 16:56:32 +0700

    first commit for task DOM with Ads

---
+++ task2-Hide_Ads_Block/script.js
@@ -0,0 +1,15 @@
+const adsContainer = document.createElement('div');
+
+adsContainer.outerHTML = `<div class="banner">
+                    <h2>Khám Phá Công Nghệ Mới Nhất!</h2>
+                    <p>Đăng ký ngay để nhận bản tin công nghệ độc quyền từ TechBit.</p>
+                    <a href="#" class="banner-btn">Đóng quảng cáo</a>
+                </div>`
+                
+const postList = document.querySelector('.content .post');
+
+
+
+
+
+
