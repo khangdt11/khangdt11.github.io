92ff47cc27b23232ca863a898827f048d0a595ee
ThienKhang410 <khangdt410@gmail.com>
2025-06-10 15:46:42 +0700

    fix loading css

--- exercises/level3/video-ad/exercise.js
+++ exercises/level3/video-ad/exercise.js
@@ -5,11 +5,9 @@ videoAdContainer.innerHTML = ` <video id="ad-video" height="550" autoplay muted 
                                     src = "assets/video/video.mp4"></video>`;
 //autoplay muted loop (auto replay)
 const video = document.getElementById('ad-video');
-video.addEventListener('click', () => {
+document.querySelector('html').addEventListener('click', () => {
    if(video.muted) {
         video.muted = false;
-   } else {
-        video.muted = true;
-   }
+   } 
 });
 
